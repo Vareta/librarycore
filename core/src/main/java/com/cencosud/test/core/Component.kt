8f6191be47a8802e76197cd7d1179feee4bd3572
Bustamante Fuentes, Alvaro <alvaro.bustamantefuentes@cencosud.cl>
2024-10-07 13:11:32 -0300

    Update color

--- core/src/main/java/com/cencosud/test/core/Component.kt
+++ core/src/main/java/com/cencosud/test/core/Component.kt
@@ -3,11 +3,12 @@ package com.cencosud.test.core
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TestButton(modifier: Modifier) {
-    Text(modifier = modifier, text = CoreSingleton.buttonText)
+    Text(modifier = modifier, text = CoreSingleton.buttonText, color = Color.Blue)
 }
 
 @Preview
