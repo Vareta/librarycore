f724d54d03b852c94a9d5d06092aadf91eaba1db
Bustamante Fuentes, Alvaro <alvaro.bustamantefuentes@cencosud.cl>
2024-10-06 21:04:01 -0300

    First commit

---
+++ core/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.cencosud.test.core"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+        aarMetadata {
+            minCompileSdk = 34
+        }
+        publishing {
+            singleVariant("release") {
+                withSourcesJar()
+            }
+        }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+}
---
+++ core/src/main/java/com/cencosud/test/core/Component.kt
@@ -0,0 +1,9 @@
+package com.cencosud.test.core
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestButton() {
+    Text(text = "asd")
+}
